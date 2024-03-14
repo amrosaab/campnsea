1fc3520fff3068134fde3e9e1cfa6c26a4a2a8de
Mohamad Tayan <raghad.tayan@gmail.com>
2024-03-13 18:22:25 -0700

    iOS meta build files.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/tayan/development/flutter/versions/3.16.8"
-export "FLUTTER_APPLICATION_PATH=/home/tayan/work/upwork/amro_saab/campnsea-main/latest/campnsea-feature-war-release"
+export "FLUTTER_ROOT=/Users/mohamadtayan/development/3.13.0"
+export "FLUTTER_APPLICATION_PATH=/Users/mohamadtayan/work/upwork/amro_saab/campnsea/app_project/campnsea"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/mohamadtayan/work/upwork/amro_saab/campnsea/app_project/campnsea/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=5.9.0"
 export "FLUTTER_BUILD_NUMBER=61"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8xYWM2MTFjNjRlYWRiZDkzYzVmNWFiYTU0OTRiOGZjM2IzNWVlOTUyLw=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/mohamadtayan/work/upwork/amro_saab/campnsea/app_project/campnsea/.dart_tool/package_config.json"
